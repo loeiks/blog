43ee7b58aea033d4c9bd06a3768b60cf2b925ec7
Enes <loeiks@icloud.com>
2026-02-20 23:05:03 +0300

    cloudflare pages adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
-import mdx from '@astrojs/mdx';
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()]
+  integrations: [mdx()],
+  adapter: cloudflare(),
 });
