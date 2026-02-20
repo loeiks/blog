4481bfd41fe07329460a9d2adb80adc8323cb138
Enes <loeiks@icloud.com>
2026-02-21 00:11:06 +0300

    first version of homepage is ready

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    // z.coerce.date() automatically converts string dates from frontmatter into JS Date objects!
+    publishDate: z.coerce.date(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
