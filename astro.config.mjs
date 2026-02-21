ee253613d6c9e3fc7f0e7ac7702e292677644659
Enes <loeiks@icloud.com>
2026-02-21 09:22:05 +0300

    sitemap adapter added with blog chunk also robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,26 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap, { ChangeFreqEnum } from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.enesb.im",
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()],
+  integrations: [
+    mdx(),
+    sitemap({
+      chunks: {
+        blog: (item) => {
+          if (/blog/.test(item.url)) {
+            return item;
+          }
+        },
+      },
+    }),
+  ],
   adapter: cloudflare(),
 });
